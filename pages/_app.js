f5d6f3af55486746a89c7e5a86b5ceedc6cc4c7b
Thiago Alves Vieira <thiago.a.vieira@hotmail.com>
2021-04-21 22:16:11 -0300

    migrating start

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import { Provider } from "react-redux";
 import { useStore } from "../store";
 import { PersistGate } from "redux-persist/integration/react";
+import "../styles/Global.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function App({ Component, pageProps }) {
   const store = useStore(pageProps.initialReduxState);
---
+++ pages/_document.js
@@ -0,0 +1,39 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/@sweetalert2/theme-material-ui@4.0.3/material-ui.min.css"
+          />
+          <script src="https://secure.mlstatic.com/sdk/javascript/v1/mercadopago.js"></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
