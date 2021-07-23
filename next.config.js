201ed3ea45052badf4656cee70affc20866739c7
SamucaBraga <samuelbgf@hotmail.com>
2021-07-23 10:19:54 -0300

    config

--- next.config.js
+++ next.config.js
@@ -9,9 +9,6 @@ module.exports = withPlugins([imagesPlugin], {
   images: {
     domains: ["storage.googleapis.com"],
   },
-  future: {
-    webpack5: true,
-  },
   env: {
     PUBLIC_KEY: "TEST-d7175695-a273-411f-aef9-2796b92731c2",
   },
