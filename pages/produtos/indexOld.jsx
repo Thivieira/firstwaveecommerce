6a6ca4b273656e635bab887b03fe1111876d7c21
Thiago Alves Vieira <thiago.a.vieira@hotmail.com>
2021-07-27 15:28:32 -0300

    filter

--- pages/produtos/indexOld.jsx
+++ pages/produtos/indexOld.jsx
@@ -1,8 +1,9 @@
 import { useContext, useEffect, useState } from "react";
-import { useRouter } from "next/router";
 import { CategoryContext } from "../../contexts/CategoryContext";
 import FadeLoader from "react-spinners/FadeLoader";
 import ReactPaginate from "react-paginate";
+
+import { useRouter } from "next/router";
 
 import { useDispatch, useSelector } from "react-redux";
 import { Drawer, Button, Alert } from "antd";
@@ -14,124 +15,33 @@ import Breadcrumb from "../../components/Breadcrumb";
 import Filter from "../../components/Filter";
 import Product from "../../components/Products/ProductCard";
 import api from "../../services/api";
-import { getFilterUrl } from "../../store/selectors/products";
 
-import {
-  getAllProducts,
-  getLoading,
-  getPaginationData,
-} from "../../store/selectors/products";
+import { getAllProducts, getLoading } from "../../store/selectors/products";
 import {
   setProducts,
   clearProducts,
   setLoading,
   sortProducts,
-  setPaginationProducts,
-  setFilterData,
-  setFilterUrl,
 } from "../../store/actions/products";
 
-export default function index() {
-  const { getCategory, setCategory } = useContext(CategoryContext);
+const products = () => {
+  const { getCategory } = useContext(CategoryContext);
+
+  const router = useRouter();
   const dispatch = useDispatch();
   const products = useSelector(getAllProducts);
+
   const loading = useSelector(getLoading);
+
   const [sort, setSort] = useState("menor");
+  const [totalPages, setTotalPages] = useState();
+  const [perPage, setPerPage] = useState();
+
   const [showFilter, setShowFilter] = useState(true);
   const [visible, setVisible] = useState(false);
+
   const [width, setWindowWidth] = useState(0);
-  const [currentPage, setCurrentPage] = useState(0);
-  const [theTotal, setTotal] = useState(0);
-  const [sizes, setSizes] = useState([]);
-  const [brands, setBrands] = useState([]);
-  const [colors, setColors] = useState([]);
-  const filterUrl = useSelector(getFilterUrl);
-  const router = useRouter();
   const { q } = router.query;
-
-  router.events.on("routeChangeStart", () => {
-    dispatch(setFilterUrl(""));
-    dispatch(setFilterData([], [], [], 0, 2000));
-  });
-
-  const paginationRedux = useSelector(getPaginationData);
-
-  const showDrawerFilters = () => setVisible(true);
-  const onCloseFilters = () => setVisible(false);
-
-  //   useEffect(() => {
-  //     let page = currentPage + 1;
-  //     dispatch(setPaginationProducts(totalPages, page, per_page, theTotal));
-  //   }, []);
-
-  //   useEffect(() => {
-  //     let page = currentPage + 1;
-  //     setCurrentPage(0);
-  //     dispatch(clearProducts());
-  //     dispatch(setLoading(false));
-  //     dispatch(setProducts(prod));
-  //     setCategory({ category: category, subcategory: subcategory, type: type });
-  //     dispatch(setPaginationProducts(totalPages, page, per_page, total));
-  //   }, [dispatch, category, subcategory, type]);
-
-  useEffect(async () => {
-    let sizeData;
-    let brandData;
-    let colorData;
-
-    if (getCategory.category && getCategory.subcategory && getCategory.type) {
-      sizeData = await api.get(
-        `/products/sizes?category=${getCategory.category}&subcategory=${getCategory.subcategory}&type=${getCategory.type}`
-      );
-      brandData = await api.get(
-        `/products/brands?category=${getCategory.category}&subcategory=${getCategory.subcategory}&type=${getCategory.type}`
-      );
-      colorData = await api.get(
-        `/products/colors?category=${getCategory.category}&subcategory=${getCategory.subcategory}&type=${getCategory.type}`
-      );
-    } else if (
-      getCategory.category &&
-      getCategory.subcategory &&
-      !getCategory.type
-    ) {
-      sizeData = await api.get(
-        `/products/sizes?category=${getCategory.category}&subcategory=${getCategory.subcategory}`
-      );
-      brandData = await api.get(
-        `/products/brands?category=${getCategory.category}&subcategory=${getCategory.subcategory}`
-      );
-      colorData = await api.get(
-        `/products/colors?category=${getCategory.category}&subcategory=${getCategory.subcategory}`
-      );
-    } else {
-      sizeData = await api.get(
-        `/products/sizes?category=${getCategory.category}`
-      );
-      brandData = await api.get(
-        `/products/brands?category=${getCategory.category}`
-      );
-      colorData = await api.get(
-        `/products/colors?category=${getCategory.category}`
-      );
-    }
-
-    setSizes(sizeData.data);
-    setBrands(brandData.data);
-    setColors(colorData.data);
-  }, []);
-
-  useEffect(async () => {
-    let page = currentPage + 1;
-    setCurrentPage(0);
-    dispatch(clearProducts());
-    dispatch(setLoading(true));
-    const { data } = await api(`/products?search=${q}&page=${page}`);
-    dispatch(setLoading(false));
-    dispatch(setProducts(data.data));
-    dispatch(
-      setPaginationProducts(data.last_page, page, data.per_page, data.total)
-    );
-  }, [currentPage, q, theTotal]);
 
   useEffect(() => {
     updateDimensions();
@@ -148,63 +58,36 @@ export default function index() {
     width < 1280 ? setShowFilter(false) : setShowFilter(true);
   });
 
+  const showDrawerFilters = () => setVisible(true);
+  const onCloseFilters = () => setVisible(false);
+
+  const [currentPage, setCurrentPage] = useState(1);
+
   const handleCloseAlert = async () => {
-    dispatch(setLoading(true));
-    dispatch(clearProducts());
-    const res = await api.get(`/products?category=${category}`);
-    const prod = res.data.data;
-    setTotal(res.data.total);
-    // totalPages = res.data.last_page;
-    dispatch(setProducts(prod));
-    dispatch(setLoading(false));
-    dispatch(setFilterUrl(""));
-    dispatch(setFilterData([], [], [], 0, 2000));
-    dispatch(
-      setPaginationProducts(
-        res.data.last_page,
-        currentPage + 1,
-        res.data.per_page,
-        res.data.total
-      )
-    );
+    // dispatch(setLoading(true));
+    // const res = useFetch(`/products?search=${q}&page=${currentPage}`);
+    // const prod = res.data.data;
+    // dispatch(setProducts(prod));
+    // dispatch(setLoading(false));
   };
 
   const changePage = ({ selected }) => {
-    setCurrentPage(selected);
+    setCurrentPage(selected + 1);
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
-  useEffect(() => {
-    let page = currentPage + 1;
-    let url = "";
-
-    if (filterUrl) {
-      // console.log("FILTER", filterUrl);
-      url = `${filterUrl}&page=${page}`;
-    } else {
-      let filterString = ``;
-      getCategory.category
-        ? (filterString += `category=${getCategory.category}`)
-        : ``;
-      getCategory.subcategory
-        ? (filterString += `&subcategory=${getCategory.subcategory}`)
-        : ``;
-      getCategory.type ? (filterString += `&type=${getCategory.type}`) : ``;
-      url = `/products?${filterString}&page=${page}`;
-    }
-
+  useEffect(async () => {
     dispatch(setLoading(true));
-    dispatch(clearProducts());
+    const { data } = await api(`/products?search=${q}&page=${currentPage}`);
+    dispatch(setProducts(data.data));
+    dispatch(setLoading(false));
+    setTotalPages(data.total);
+    setPerPage(data.per_page);
+  }, [currentPage, q, perPage, totalPages]);
 
-    api.get(url).then(({ data }) => {
-      dispatch(setLoading(false));
-      dispatch(setProducts(data.data));
-      setTotal(data.total);
-      dispatch(
-        setPaginationProducts(data.last_page, page, data.per_page, data.total)
-      );
-    });
-  }, [currentPage, dispatch]);
+  const pages = totalPages / perPage;
+
+  // console.log(perPage, pages, totalPages)
 
   const menu = (
     <Menu value={sort} onClick={(obj) => handleChangeSort(obj.item)}>
@@ -246,7 +129,7 @@ export default function index() {
         <div className="filter-sort">
           {products.length > 0 && (
             <>
-              <p>{paginationRedux.theTotal} Produto(s) Encontrados</p>
+              <p>{products.length} Produto(s) Encontrados</p>
 
               <Dropdown overlay={menu}>
                 <a
@@ -269,13 +152,12 @@ export default function index() {
                   key={removeIdDuplicate(product.id)}
                 />
               ))}
-              {paginationRedux.theTotal > 15 ? (
+              {totalPages > 15 ? (
                 <ReactPaginate
                   previousLabel={"<"}
                   nextLabel={">"}
-                  pageCount={paginationRedux.totalPages}
+                  pageCount={pages}
                   onPageChange={(selected) => changePage(selected)}
-                  forcePage={currentPage}
                   containerClassName={"paginationsBttn"}
                   previousLinkClassName={"previousBttn"}
                   nextLinkClassName={"nextBttn"}
@@ -318,9 +200,6 @@ export default function index() {
                 category={getCategory.category}
                 subcategory={getCategory.subcategory}
                 type={getCategory.type}
-                brands={brands}
-                sizes={sizes}
-                colors={colors}
               />
             </>
           ) : (
@@ -347,9 +226,6 @@ export default function index() {
                     category={getCategory.category}
                     subcategory={getCategory.subcategory}
                     type={getCategory.type}
-                    brands={brands}
-                    sizes={sizes}
-                    colors={colors}
                   />
                 </div>
               </Drawer>
@@ -359,4 +235,6 @@ export default function index() {
       </div>
     </>
   );
-}
+};
+
+export default products;
