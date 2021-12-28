c7cde36746c165bd6c64beaed7325f9ed0fe7a23
Thiago Alves Vieira <thiago.a.vieira@hotmail.com>
2021-12-28 17:50:36 -0300

    no ssr in products page

--- pages/produtos/[...param].jsx
+++ pages/produtos/[...param].jsx
@@ -36,75 +36,7 @@ import {
 import { removeIdDuplicate } from "../../helpers";
 import FilterSort from "../../components/FilterSort";
 
-export async function getStaticPaths() {
-  let paths = [
-    { params: { param: ["surf"] } },
-    { params: { param: ["masculino"] } },
-    { params: { param: ["feminino"] } },
-    { params: { param: ["juvenil"] } },
-    { params: { param: ["calcado"] } },
-    { params: { param: ["acessorio"] } },
-    { params: { param: ["acessorio", "oculos"] } },
-    { params: { param: ["acessorio", "relogio"] } },
-    { params: { param: ["surf", "wetsuit"] } },
-    { params: { param: ["surf", "quilha"] } },
-    { params: { param: ["surf", "leash"] } },
-    { params: { param: ["surf", "lycra"] } },
-    { params: { param: ["surf", "prancha"] } },
-    { params: { param: ["surf", "capa"] } },
-    { params: { param: ["surf", "deck"] } },
-    { params: { param: ["surf", "bone"] } },
-    { params: { param: ["masculino", "vestuario"] } },
-    { params: { param: ["masculino", "vestuario", "bermuda"] } },
-    { params: { param: ["masculino", "vestuario", "camiseta"] } },
-    { params: { param: ["masculino", "vestuario", "camisa"] } },
-    { params: { param: ["masculino", "vestuario", "regata"] } },
-    { params: { param: ["masculino", "vestuario", "calca"] } },
-    { params: { param: ["masculino", "vestuario", "jaqueta"] } },
-    { params: { param: ["masculino", "vestuario", "sunga"] } },
-    { params: { param: ["masculino", "acessorio"] } },
-    { params: { param: ["masculino", "acessorio", "bone"] } },
-    { params: { param: ["masculino", "acessorio", "mochila"] } },
-    { params: { param: ["masculino", "acessorio", "carteira"] } },
-    { params: { param: ["masculino", "acessorio", "cinto"] } },
-    { params: { param: ["masculino", "acessorio", "pochete"] } },
-    { params: { param: ["masculino", "acessorio", "gorro"] } },
-    { params: { param: ["masculino", "acessorio", "meia"] } },
-    { params: { param: ["feminino", "vestuario"] } },
-    { params: { param: ["feminino", "vestuario", "short"] } },
-    { params: { param: ["feminino", "vestuario", "saia"] } },
-    { params: { param: ["feminino", "vestuario", "calca"] } },
-    { params: { param: ["feminino", "vestuario", "camiseta"] } },
-    { params: { param: ["feminino", "vestuario", "regata"] } },
-    { params: { param: ["feminino", "vestuario", "vestido"] } },
-    { params: { param: ["feminino", "vestuario", "macaquinho"] } },
-    { params: { param: ["feminino", "vestuario", "body"] } },
-    { params: { param: ["feminino", "vestuario", "jaqueta"] } },
-    { params: { param: ["feminino", "vestuario", "biquini"] } },
-    { params: { param: ["feminino", "acessorio"] } },
-    { params: { param: ["feminino", "acessorio", "pochete"] } },
-    { params: { param: ["feminino", "acessorio", "mochila"] } },
-    { params: { param: ["feminino", "acessorio", "bone"] } },
-    { params: { param: ["feminino", "acessorio", "cinto"] } },
-    { params: { param: ["feminino", "acessorio", "carteira"] } },
-    { params: { param: ["feminino", "acessorio", "gorro"] } },
-    { params: { param: ["feminino", "acessorio", "necessaire"] } },
-    { params: { param: ["feminino", "acessorio", "meia"] } },
-    { params: { param: ["juvenil", "vestuario"] } },
-    { params: { param: ["juvenil", "vestuario", "camiseta"] } },
-    { params: { param: ["juvenil", "vestuario", "regata"] } },
-    { params: { param: ["juvenil", "vestuario", "bermuda"] } },
-    { params: { param: ["juvenil", "vestuario", "calca"] } },
-    { params: { param: ["juvenil", "vestuario", "jaqueta"] } },
-  ];
-
-  return {
-    paths,
-    fallback: "blocking",
-  };
-}
-
-export async function getStaticProps(ctx) {
+export async function getServerSideProps(ctx) {
   let category = ctx.params.param[0];
   let subcategory = ctx.params.param[1];
   let type = ctx.params.param[2];
