e31218986060e766f7041057c3191f12e41dcf8e
Thiago Alve Vieira <thiago.a.vieira@hotmail.com>
2022-04-08 10:50:33 -0300

    build cache

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-const nextBuildId = require('next-build-id')
 const withPlugins = require('next-compose-plugins')
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
@@ -13,8 +12,7 @@ const nextConfiguration = {
     // Warning: Dangerously allow production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true
-  },
-  generateBuildId: async () => nextBuildId({ dir: __dirname })
+  }
 }
 
 module.exports = withPlugins([withBundleAnalyzer], nextConfiguration)
